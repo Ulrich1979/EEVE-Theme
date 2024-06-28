883464a8b9b32de8ae0e6e52bc9943b778f522c9
Ulrich1979 <141607646+Ulrich1979@users.noreply.github.com>
2024-06-28 17:02:46 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-/*  MOBILE MENU */
-
-let menuBtn = document.getElementById("mobile-menu")
-let fullNav = document.getElementById("full-nav")
-
-menuBtn.addEventListener("click", function() {
-    fullNav.className = fullNav.className === "mobile" ? "desktop" : "mobile"
-})
